5f790053c0b6774c0664df11fe57d3a471387fc7
Jesús Hernández <jahl-v2@hotmail.com>
2023-07-14 10:59:20 -0600

    rework main function file to use the new plenty client class

--- app.rb
+++ app.rb
@@ -1,34 +1,20 @@
 # frozen_string_literal: true
 
+require 'active_record'
 require 'functions_framework'
 require 'dotenv'
-require 'faraday'
-require 'json'
-
 Dotenv.load
 
-def connection
-  Faraday.new(url: ENV['BASE_URL']) do |faraday|
-    faraday.headers['Authorization'] = "Bearer #{ENV['TOKEN']}"
-    faraday.adapter Faraday.default_adapter
-  end
-end
+require_relative './lib/plenty_client'
+require_relative './config/database'
 
-def plenty_client_get(url, params={})
-  response = connection.get url, params
-  JSON.parse(response.body)
-end
-
-def plenty_client_delete(url)
-  response = connection.delete url
-  JSON.parse(response.body)
-end
 
 FunctionsFramework.http "delete_images" do |_request|
+  client = PlentyClient.new
   item_id = 1365220
   url = "items/#{item_id}/images"
   delete_url = "images"
-  images = plenty_client_get(url)
+  images = client.get(url)
 
   images.each do |image|
     puts "id: #{image['id']}, url: #{image['url']}"
