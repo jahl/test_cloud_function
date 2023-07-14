a0d2fa9e55808af9a48abd171a18e27413e2495a
Jesús Hernández <jahl-v2@hotmail.com>
2023-07-14 12:58:04 -0600

    refactor the delete_images function to work with any valid article no

--- app.rb
+++ app.rb
@@ -2,12 +2,16 @@
 require_relative './config/init'
 
 FunctionsFramework.http "delete_images" do |_request|
+  article_no = "M9-R51400"
   client = PlentyClient.new
-  item_id = 1365220
-  url = "items/#{item_id}/images"
-  delete_url = "images"
-  images = client.get(url)
 
+  article = Buddy::Article.find(:first, params: {article_no: article_no})
+  raise "Article #{article_no}" if article.nil?
+
+  pm_item_id = article.plenty_mapping.pm_item_id
+  puts "Found article: #{article_no} with PM item id of: #{pm_item_id}"
+
+  images = client.get("items/#{pm_item_id}/images")
   images.each do |image|
     puts "id: #{image['id']}, url: #{image['url']}"
   end
